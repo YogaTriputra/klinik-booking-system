b11d7aa7ea3a74d99485a746994cfc9b2654dad0
mzakiydn <m.zakiyuddin@students.amikom.ac.id>
2026-06-15 17:14:52 +0700

    push up

--- Zaki/linkedlist.cpp
+++ Zaki/linkedlist.cpp
@@ -8,9 +8,32 @@ struct Node {
 Node* head = nullptr;
 
 void tambahNodePasien(Pasien pasien) {
-
+    Node* nodeBaru = new Node();
+    nodeBaru->data = pasien;
+    nodeBaru->next = nullptr;
+    
+    if (head == nullptr) {
+        head = nodeBaru;
+    } else {
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = nodeBaru;
+    }
 }
 
 void tampilLinkedList() {
-
+    if (head == nullptr) {
+        cout << "\n[Info] Belum ada pasien yang masuk rekam medis tetap.\n";
+        return;
+    }
+    
+    Node* temp = head;
+    cout << "\n=== Data Pasien Selesai ===\n";
+    while (temp != nullptr) {
+        cout << "[ID: " << temp->data.idPasien << " -> Nama: " << temp->data.nama << "] -> ";
+        temp = temp->next;
+    }
+    cout << "NULL\n";
 }

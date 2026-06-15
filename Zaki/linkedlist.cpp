9ee0bf606c736ddb7edecf2c82097db445000f90
mzakiydn <m.zakiyuddin@students.amikom.ac.id>
2026-06-15 15:15:39 +0700

    Kerangka File:

---
+++ Zaki/linkedlist.cpp
@@ -0,0 +1,16 @@
+#include "../include/pasien.h"
+
+struct Node {
+    Pasien data;
+    Node* next;
+};
+
+Node* head = nullptr;
+
+void tambahNodePasien(Pasien pasien) {
+
+}
+
+void tampilLinkedList() {
+
+}
